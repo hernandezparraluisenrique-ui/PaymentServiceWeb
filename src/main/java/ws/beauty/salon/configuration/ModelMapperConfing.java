0cd3f7618d302951c37e8b31471bd761a4ad91dd
Lupita-Hernandez <lupita27hv@gmail.com>
2025-10-06 21:40:19 -0600

    First meeting of the beauty salon project

---
+++ src/main/java/ws/beauty/salon/configuration/ModelMapperConfing.java
@@ -0,0 +1,12 @@
+package ws.beauty.salon.configuration;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfing {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
