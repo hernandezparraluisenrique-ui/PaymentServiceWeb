49369a63ac28e52003214e5693285a5edf04b38f
hernandezparraluisenrique-ui <hernandezparraluisenrique@gmail.com>
2025-12-02 08:40:08 -0600

    Descripción breve de los cambios

--- src/main/java/client/AppointmentGatewayClient.java
+++ src/main/java/client/AppointmentGatewayClient.java
@@ -1,12 +1,27 @@
 package client;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
 import ws.beauty.salon.dto.AppointmentResponse;
 
-@FeignClient(name = "api-gateway")   
+@FeignClient(
+        name = "api-gateway",
+        url = "${gateway.url}" // http://localhost:8080 o Render URL
+)  
 public interface AppointmentGatewayClient {
-    @GetMapping("/api/v1/core/appointments/{id}")
-    AppointmentResponse getAppointmentById(@PathVariable("id") Integer id);
+    @GetMapping("/core/api/v1/appointments/{id}")
+    AppointmentResponse getAppointment(@PathVariable Integer id);
+
+    @GetMapping("/core/api/v1/appointments/client/{clientId}")
+    List<AppointmentResponse> getAppointmentsByClient(@PathVariable Integer clientId);
+
+    @GetMapping("/core/api/v1/appointments/stylist/{stylistId}")
+    List<AppointmentResponse> getAppointmentsByStylist(@PathVariable Integer stylistId);
+    
+    AppointmentResponse getAppointmentById(Integer appointmentId);
+
 }
